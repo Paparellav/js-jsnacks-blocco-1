cdfbf6471bcc4f9bea092d8ec5f1a15c29a438fb
Paparellav <vitantoniopaparella@gmail.com>
2022-04-19 14:44:20 +0200

    jsnack4 terminato

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -3,12 +3,25 @@
 // e calcola la somma di tutte le cifre che compongono il numero.
 
 //chiedere all'utente di inserire un numero di 4 cifre
-for (let i = 0; i <= 1; i++) {
-    let userNumber = prompt('Inserisci un numero di 4 cifre');
+
+// for (let i = 0; i <= 1; i++) {
+//     let userNumber = prompt('Inserisci un numero di 4 cifre');
     
-    if (userNumber > 999 && userNumber < 10000) {
-        console.log(userNumber, typeof(userNumber));
-    } else {
-        alert('Il numero inserito non ha 4 caratteri. Tentativi rimasti: 1');
-    }
+//     if (userNumber > 999 && userNumber < 10000) {
+//         console.log(userNumber, typeof(userNumber));
+//     } else {
+//         alert('Il numero inserito non ha 4 caratteri. Tentativi rimasti: 1');
+//     }
+// }
+
+let userNumber = parseInt(prompt('Inserisci un numero di 4 cifre'));
+console.log(userNumber, typeof (userNumber));
+
+let sumOfNumber = 0;
+
+while (userNumber) {
+    sumOfNumber += userNumber % 10;
+    userNumber = Math.floor(userNumber / 10);
 }
+
+console.log(sumOfNumber);
